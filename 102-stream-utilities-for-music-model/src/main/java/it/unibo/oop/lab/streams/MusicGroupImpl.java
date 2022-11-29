46ce5b66fac39da6772b9de9e7206cff561e08c9
Emanuele Dajko <ema.unibo@gmail.com>
2022-11-29 23:30:48 +0100

    error: need to be solved!

--- 102-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 102-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.streams;
 
 import java.security.Key;
+import java.security.KeyStore.Entry;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -83,7 +85,11 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Optional<String> longestAlbum() {
-        return null;
+        return this.songs.stream().filter(a -> a.getAlbumName().isPresent())
+        .collect(Collectors.groupingBy(Song::getAlbumName, Collectors.summingDouble(Song::getDuration)))
+        .entrySet().stream()
+        .collect(Collectors.maxBy(Comparator.comparingDouble(Entry::getValue)))
+        .flatMap(Entry::getKey);
     }
 
     private static final class Song {
